11e7a6d87b37483617f41d34ca10be68bff57f9c
marvin <marvinsilc@protonmail.ch>
2025-01-07 19:37:33 -0300

    Testing

--- src/br/com/dominio/universidade/MainUniversidade.java
+++ src/br/com/dominio/universidade/MainUniversidade.java
@@ -5,7 +5,7 @@ public class MainUniversidade
 {
     public static void main (String [] args)
     {
-        System.out.println("asdfaf");
+
     }//Fim do Método Main / End of Main Method;
 
 }//Fim da Classe / End of Class;
